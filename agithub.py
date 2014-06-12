616967b9530e3f2b4100ee51331e9f88129a2f7b
Jonathan Paugh <jpaugh@gmx.us>
2014-06-12 11:28:48 -0400

    Version bump v1.2
    Should have done this when splitting off the maint branch, but alas!

--- agithub.py
+++ agithub.py
@@ -15,7 +15,7 @@ else:
     import urllib as urllib
     urllib.parse = urllib
 
-VERSION = [1,1]
+VERSION = [1,2]
 STR_VERSION = 'v' + '.'.join(str(v) for v in VERSION)
 
 # These headers are implicitly included in each request; however, each
