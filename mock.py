2091f081677671e05e1b5ec386fa0d885f1335cb
Jonathan Paugh <jpaugh@gmx.us>
2015-05-10 19:41:14 +0000

    New unit tests for RequestBuilder

---
+++ mock.py
@@ -0,0 +1,20 @@
+# Mock objects for testing
+class Client(object):
+    http_methods = ('demo', 'test')
+
+    def __init__(self, username=None, password=None, token=None,
+            connection_properties=None):
+        pass
+
+    def setConnectionProperties(self, props):
+        pass
+
+    def demo(self, *args,**params):
+        return self.methodCalled('demo', *args, **params)
+
+    def test(self, *args,**params):
+        return self.methodCalled('test', *args, **params)
+
+    def methodCalled(self, methodName, *args, **params):
+        return { 'methodName' : methodName, 'args' : args
+               , 'params' : params }
