de8084777e74c205013471da4a40793c6f7fc395
Jared Hobbs <jared@pyhacker.com>
2016-01-12 09:34:15 -0800

    add more info for the cheese shop

--- setup.py
+++ setup.py
@@ -4,4 +4,17 @@ from distutils.core import setup
 setup(name='agithub',
       version='1.3',
       author='Jonathan Paugh',
-      py_modules=['agithub'])
+      url='https://github.com/jaredhobbs/agithub',
+      description="The agnostic Github API. It doesn't know, "
+                  "and you don't care.",
+      py_modules=['agithub'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 3',
+          'Topic :: Utilities',
+      ],
+      keywords=['github', 'api'])
