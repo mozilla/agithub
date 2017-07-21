4f6151497d14faa078a80ff3856936e4725b0b3d
Jean-Paul Balabanian <jepebe@users.noreply.github.com>
2017-07-21 12:44:33 +0200

    Added support for url_postfix in ConnectionProperties This is to support the way, for example, Jenkins provides an API: i.e.: www.url.com/view/All/api/json

--- agithub/agithub_test.py
+++ agithub/agithub_test.py
@@ -2,9 +2,10 @@
 # Copyright 2012-2016 Jonathan Paugh and contributors
 # See COPYING for license details
 from agithub.GitHub import GitHub
-from agithub.base import IncompleteRequest
-import mock
+from agithub.base import IncompleteRequest, ConnectionProperties
+import agithub.mock as mock
 import unittest
+
 
 class TestGitHubObjectCreation(unittest.TestCase):
     def test_user_pw(self):
@@ -28,7 +29,6 @@ class TestGitHubObjectCreation(unittest.TestCase):
 
 
 class TestIncompleteRequest(unittest.TestCase):
-
     def newIncompleteRequest(self):
         return IncompleteRequest(mock.Client())
 
@@ -40,30 +40,61 @@ class TestIncompleteRequest(unittest.TestCase):
     def test_callMethodDemo(self):
         rb = self.newIncompleteRequest()
         self.assertEqual(rb.path.demo(),
-                { "methodName" : "demo"
-                , "args" : ()
-                , "params" : { "url" : "/path" }
-                })
+                         {
+                             "methodName": "demo",
+                             "args": (),
+                             "params": {"url": "/path"}
+                         })
+
     def test_pathByGetItem(self):
         rb = self.newIncompleteRequest()
         rb["hug"][1]["octocat"]
         self.assertEqual(rb.url, "/hug/1/octocat")
 
-    def test_callMethodDemo(self):
-        rb = self.newIncompleteRequest()
-        self.assertEqual(rb.path.demo(),
-                { "methodName" : "demo"
-                , "args" : ()
-                , "params" : { "url" : "/path" }
-                })
-
     def test_callMethodTest(self):
         rb = self.newIncompleteRequest()
         self.assertEqual(rb.path.test(),
-                { "methodName" : "test"
-                , "args" : ()
-                , "params" : { "url" : "/path" }
-                })
+                         {
+                             "methodName": "test",
+                             "args": (),
+                             "params": {"url": "/path"}
+                         })
+
+
+class TestConnectionProperties(unittest.TestCase):
+    def test_url_prefix(self):
+        cp = ConnectionProperties(url_prefix='/pre/fix')
+
+        self.assertIsNotNone(cp.url_prefix)
+        self.assertIsNone(cp.url_postfix)
+
+        url = cp.constructUrl('/find/files')
+        self.assertEqual(url, '/pre/fix/find/files')
+
+    def test_url_postfix(self):
+        cp = ConnectionProperties(url_postfix='/post/fix')
+
+        self.assertIsNone(cp.url_prefix)
+        self.assertIsNotNone(cp.url_postfix)
+
+        url = cp.constructUrl('/find/files')
+        self.assertEqual(url, '/find/files/post/fix')
+
+    def test_url_prefix_and_url_postfix(self):
+        cp = ConnectionProperties(url_prefix='/pre/fix', url_postfix='/post/fix')
+
+        self.assertIsNotNone(cp.url_prefix)
+        self.assertIsNotNone(cp.url_postfix)
+
+        url = cp.constructUrl('/find/files')
+        self.assertEqual(url, '/pre/fix/find/files/post/fix')
+
+    def test_url_postfix_with_parameters(self):
+        cp = ConnectionProperties(url_postfix='/post/fix')
+
+        url = cp.constructUrl('/find/files?key=value')
+        self.assertEqual(url, '/find/files/post/fix?key=value')
+
 
 if __name__ == '__main__':
     unittest.main()
